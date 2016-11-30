584b60effcafd17c50a4a2655373e64e8bc02a69
Tejas Sharma <tejas.s1996@gmail.com>
2016-11-29 21:10:10 -0600

    added some group creation files

---
+++ app/groupme.py
@@ -0,0 +1,45 @@
+from groupy import Bot, Group
+from app import db, models
+
+def add_job_to_group(job, group):
+    """
+    Takes a Job and a Group (as defined in models.py)
+    and adds the job to the group
+    """
+    categories=models.get_active_categories()
+    employer = job.employer
+    bot = groupy.Bot.list().filter(bot_id=group.bot_id).first
+    if bot is None or employer is None:
+        return False
+
+    message_string = '''Hey everyone! A new job has been posted.
+{} would like you to do something.
+Location: {}
+Description: {}
+Compensation: {}
+Category: {}
+Please visit {} to accept this job'''.format(employer.name,
+                                             job.location,
+                                             job.description,
+                                             job.compensation,
+                                             categories[job.category_id],
+                                             url_for('accept_job', job_id=job.id, _external=True))
+    
+    bot.post(message_string)
+    return True
+
+def create_group(group_name, creator_id):
+    """
+    Creates a group, and a bot for that group, and then adds the group to the app database
+    """
+
+    new_group = Group.create(group_name, share=True)
+    new_bot = Bot.create("JobPostBot", new_group.group_id)
+    db_group = models.Group(group_name=new_group.name,
+                            group_id=new_group.group_id,
+                            bot_name=new_bot.name,
+                            bot_id=new_bot.bot_id,
+                            active=True,
+                            creator_id=creator_id)
+    db.session.add(db_group)
+    db.session.commit()
